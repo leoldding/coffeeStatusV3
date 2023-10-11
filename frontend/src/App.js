ff8e9ed6ff734eb525132ae250485c35e7fb6219
Leo Ding <51973536+leoldding@users.noreply.github.com>
2023-10-11 14:04:45 -0400

    moved assets to public folder (#12)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,16 @@ import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import CoffeeMain from './components/coffee/js/Main.js'
 import CoffeeAdmin from './components/coffee/js/Admin.js'
-import coffee_cup from "./assets/coffee_cup.png";
 import "./styles.css";
 
 function App() {
     // set index elements
     useEffect(() => {
         let icon = document.getElementById("icon")
-        icon.href = coffee_cup
+        icon.href = process.env.PUBLIC_URL + "coffee_cup.png"
 
         let apple_icon = document.getElementById("apple_icon")
-        apple_icon.href = coffee_cup
+        apple_icon.href = process.env.PUBLIC_URL + "coffee_cup.png"
 
         document.body.className = "bg-coffeeWhite-1 text-coffeeBlack-1";
     }, []);
