234787dded8c1462ecbe59eedaa189f641141d05
leoldding <leo.ld.ding@gmail.com>
2023-10-26 13:30:29 -0400

    moved footer links to right

--- frontend/src/components/coffee/js/Footer.js
+++ frontend/src/components/coffee/js/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function CoffeeFooter() {
 
     return (
-        <div className={"relative bottom-0 w-full h-[36px] bg-[#9C6F44] flex flex-row justify-start items-center space-x-2 text-coffeeWhite-1 px-4"}>
+        <div className={"relative bottom-0 w-full h-[36px] bg-[#9C6F44] flex flex-row justify-end items-center space-x-2 text-coffeeWhite-1 px-4"}>
             <Link to={"/"}>Home</Link>
             <div>|</div>
             <Link to={"/admin"}>Admin</Link>
